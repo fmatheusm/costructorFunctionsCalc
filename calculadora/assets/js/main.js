459366232b9d623348270f495a172188b64c4cfc
Matheus Martins <matheuss_fernandes@outlook.com>
2025-03-27 18:13:52 -0300

    Calculator

---
+++ calculadora/assets/js/main.js
@@ -0,0 +1,71 @@
+function CriaCalculadora() {
+
+    this.display = document.querySelector('.display');
+
+    this.inicia = () => {
+        this.clickBotoes();
+        this.pressionaEnter();
+    }
+
+    this.pressionaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if (e.keyCode === 13) {
+                this.realizaConta();
+            }
+        })
+    }
+
+    this.btnParaDisplay = (valor) => {
+        this.display.value += valor;
+    }
+
+    this.clearDisplay = () => {
+        this.display.value = '';
+    }
+
+    this.apagaUm = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    }
+
+    this.realizaConta = () => {
+        let conta = this.display.value;
+
+        try {
+            conta = eval(conta);
+            if (!conta && conta !== 0) {
+                alert('Conta Invalida');
+                return;
+            }
+
+            this.display.value = conta;
+        } catch (e) {
+            alert('Conta Invalida');
+            return;
+        }
+    }
+
+    this.clickBotoes = () => {
+        document.addEventListener('click', e => {
+            const el = e.target;
+
+            if (el.classList.contains('btn-num')) {
+                this.btnParaDisplay(el.innerText);
+            }
+
+            if (el.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+
+            if (el.classList.contains('btn-del')) {
+                this.apagaUm();
+            }
+
+            if (el.classList.contains('btn-eq')) {
+                this.realizaConta();
+            }
+        });
+    }
+}
+
+const calculadora = new CriaCalculadora();
+calculadora.inicia();
